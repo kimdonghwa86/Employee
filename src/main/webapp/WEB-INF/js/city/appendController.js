a180219ba8186ee9d9cd55cb38b3c90cc1750e00
kimdh <iiehdghkii@naver.com>
2015-12-15 15:18:10 +0900

    151215

--- src/main/webapp/WEB-INF/js/city/appendController.js
+++ src/main/webapp/WEB-INF/js/city/appendController.js
@@ -12,10 +12,25 @@ app.config(function($routeProvider) {
 		
 	});
 
-app.controller('appendController', function($scope, $http,URL) {
+app.controller('appendController', function($scope, $http, $location,URL) {
 	console.log("appendController....");
-	console.log("URL.POST_ITEM_APPEND_BASE = " + URL.POST_ITEM_APPEND_BASE);
+	console.log("URL.POST_ITEM_APPEND = " + URL.POST_ITEM_APPEND);
 	
 	$scope.$parent.title = "append City View";
-	
+	$scope.submit = function() {
+		var ajax = $http.post(URL.POST_ITEM_APPEND,{
+			name        : $scope.city.name,
+			district    : $scope.city.district,
+			countryCode : $scope.city.countryCode,
+			population  : $scope.city.population
+		});
+		
+		ajax.then(function(value){
+			$location.path("/list");
+		}, function(reason){
+			console.dir(reason);
+			$scope.city = reason.data;
+		})
+	};
+
 });
